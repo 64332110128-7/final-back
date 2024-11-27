18e598d032626a9d7143dc9dfaca432b8994bacd
Suttipong Thaoun <suttipong.ta@rmuti.ac.th>
2024-11-28 01:07:01 +0700

    1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.json({ message: "Server is working fine!" });
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Server run in port" + " " + port);
+});
