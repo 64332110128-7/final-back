d63d8081fc35d57ebd4ab994d8ab74e688957007
Suttipong Thaoun <suttipong.ta@rmuti.ac.th>
2025-02-18 02:17:02 +0700

    add latitude longitude

---
+++ prisma/migrations/20250217191642_add/migration.sql
@@ -0,0 +1,131 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `userId` INTEGER NOT NULL AUTO_INCREMENT,
+    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
+    `password` VARCHAR(191) NOT NULL,
+    `firstName` VARCHAR(191) NULL,
+    `lastName` VARCHAR(191) NULL,
+    `phone` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Plan` (
+    `planId` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `budget` DOUBLE NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `locationId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`planId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Location` (
+    `locationId` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(5000) NOT NULL,
+    `categoryId` INTEGER NOT NULL,
+    `map` VARCHAR(3000) NULL,
+    `address` VARCHAR(3000) NULL,
+    `phone` VARCHAR(191) NULL,
+    `date` VARCHAR(191) NULL,
+    `budget` ENUM('LOW', 'MEDIUM', 'HIGH') NULL,
+    `price` INTEGER NULL,
+    `latitude` DOUBLE NULL,
+    `longitude` DOUBLE NULL,
+    `average_score` DOUBLE NULL,
+
+    PRIMARY KEY (`locationId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `categoryId` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`categoryId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `LocationImg` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `url` VARCHAR(191) NOT NULL,
+    `filename` VARCHAR(191) NULL,
+    `locationId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `LocationImg_filename_key`(`filename`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `LocationScore` (
+    `scoreId` INTEGER NOT NULL AUTO_INCREMENT,
+    `score` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `locationId` INTEGER NOT NULL,
+    `commentId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `LocationScore_userId_locationId_key`(`userId`, `locationId`),
+    PRIMARY KEY (`scoreId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `LocationComment` (
+    `commentId` INTEGER NOT NULL AUTO_INCREMENT,
+    `text` VARCHAR(3000) NOT NULL,
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+    `locationId` INTEGER NOT NULL,
+    `parentId` INTEGER NULL,
+
+    PRIMARY KEY (`commentId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `LocationCommentImg` (
+    `imgId` INTEGER NOT NULL AUTO_INCREMENT,
+    `url` VARCHAR(191) NOT NULL,
+    `filename` VARCHAR(191) NULL,
+    `commentId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `LocationCommentImg_filename_key`(`filename`),
+    PRIMARY KEY (`imgId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Plan` ADD CONSTRAINT `Plan_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Plan` ADD CONSTRAINT `Plan_locationId_fkey` FOREIGN KEY (`locationId`) REFERENCES `Location`(`locationId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Location` ADD CONSTRAINT `Location_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`categoryId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LocationImg` ADD CONSTRAINT `LocationImg_locationId_fkey` FOREIGN KEY (`locationId`) REFERENCES `Location`(`locationId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LocationScore` ADD CONSTRAINT `LocationScore_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LocationScore` ADD CONSTRAINT `LocationScore_locationId_fkey` FOREIGN KEY (`locationId`) REFERENCES `Location`(`locationId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LocationScore` ADD CONSTRAINT `LocationScore_commentId_fkey` FOREIGN KEY (`commentId`) REFERENCES `LocationComment`(`commentId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LocationComment` ADD CONSTRAINT `LocationComment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LocationComment` ADD CONSTRAINT `LocationComment_locationId_fkey` FOREIGN KEY (`locationId`) REFERENCES `Location`(`locationId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LocationComment` ADD CONSTRAINT `LocationComment_parentId_fkey` FOREIGN KEY (`parentId`) REFERENCES `LocationComment`(`commentId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LocationCommentImg` ADD CONSTRAINT `LocationCommentImg_commentId_fkey` FOREIGN KEY (`commentId`) REFERENCES `LocationComment`(`commentId`) ON DELETE CASCADE ON UPDATE CASCADE;
